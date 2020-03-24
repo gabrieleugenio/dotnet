ba8f3ea6fcd188ee571517779dd2919f2298c880
Gabriel Eugenio <gabriel.eugenio@dbccompany.com.br>
2020-03-24 14:59:35 -0300

    feat(version): implement api  versioning.

--- RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy/Startup.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using RestWithASPNETUdemy.Services;
+using RestWithASPNETUdemy.Services.Implementattions;
+using RestWithASPNETUdemy.Model.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace RestWithASPNETUdemy
 {
@@ -25,22 +26,20 @@ namespace RestWithASPNETUdemy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+            services.AddMvc();
+            services.AddApiVersioning();
+            //Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImpl>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
 
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
